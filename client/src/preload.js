9d6ba1b1cf694bc96dfafa094e3f96f360bb9ce7
burakacar48 <136010277+burakacar48@users.noreply.github.com>
2025-10-19 12:51:03 +0300

    panel

--- client/src/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  launchGame: (game) => ipcRenderer.send('launch-game', game),
-  unzipSave: (args) => ipcRenderer.invoke('unzip-save', args),
-  zipSave: (args) => ipcRenderer.invoke('zip-save', args)
-});
